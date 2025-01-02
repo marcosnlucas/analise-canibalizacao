178546dcb1f8cbbbd5e1564134c828fe715aa341
Marcos Nunes Lucas <marcosnuneslucas@MacBook-Air-de-Arklok-4.local>
2025-01-02 19:24:08 -0300

    Ajustes para deploy na Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/analise-de-canibalizacao/', // Caminho específico no tools.seoz.com.br
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
